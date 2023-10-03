51006552d94fd35888c4038843ff5d815f956e63
Mindaugas <137099858+MindBil@users.noreply.github.com>
2023-10-03 21:02:44 +0300

    First 5 steps from CAO completed

---
+++ 1-p-cao-tasks/index.js
@@ -0,0 +1,7 @@
+// console.log("Hello")
+
+import casual from "casual";
+
+const randomCity = casual.city;
+
+console.log("Random City:", randomCity);
---
+++ 1-p-cao-tasks/randomCity.js
@@ -0,0 +1,5 @@
+// const casual = require("casual");
+
+// const randomCity = casual.city;
+
+// console.log("Random City:", randomCity);
