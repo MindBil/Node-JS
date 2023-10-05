5f861f22f8ef80cf38c93251dcbea392036d1794
Mindaugas <137099858+MindBil@users.noreply.github.com>
2023-10-05 21:24:21 +0300

    Nodemon tested

--- 1-p-node-express-test/index.js
+++ 1-p-node-express-test/index.js
@@ -2,9 +2,19 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const users = ["Rokas", "Tomas", "Gedas"];
+
 app.get("/", (req, res) => {
     res.send("Hello world");
 });
+
+app.get("/api/users", (req, res) => {
+    res.send(users);
+});
+
+app.get("/fruits", (req, res) => {
+    res.send(["Apple", "banana"]);
+})
 
 app.listen(port, () => {
     console.log(`App listening on ${port}`);
